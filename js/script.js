0ece926ca5a7448ea2abf258529ad42ba2e1b738
Damien Jiang <damieneskimo@gmail.com>
2015-11-11 00:54:35 +1300

    use bootstrap to make the responsive layout and some feature like modal, accordion, carousel, media, form

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function() {
+    //change the slides interval time to 4 seconds
+    $(".carousel").carousel({
+        interval: 4000,
+    });
+    
+    //make menus drop automatically 
+    $("ul.nav li.dropdown").hover(function() {
+        $(".dropdown-menu", this).fadeIn();
+    }, function() {
+        $(".dropdown-menu", this).fadeOut("fast");
+    }); //hover
+    
+
+})
